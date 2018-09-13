503f176628271ff8c13662a157aef55a57668ddc
ma9arona <chkoupistane@gmail.com>
2018-09-13 10:46:53 +0100

    swip

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated, Easing } from 'react-native';
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
 
-export default class HomeScreen extends React.Component { 
-    render() { 
+export default class HomeScreen extends React.Component {
+
+    state = { height : new Animated.Value(0) };
+
+    onSwipeUp(state) {
+      Animated.timing(this.state.height, {
+        toValue : 1,
+        duration : 300,
+        easing : Easing.inOut(Easing.quad),
+      }).start();
+    }
+
+    onSwipe(direction, state) {
+      const { SWIPE_UP } = swipeDirections;
+
+      switch(direction) {
+        case SWIPE_UP:
+          this.onSwipeUp(state);
+          break;
+      }
+
+    }
+
+    render() {
         return (
-            <View style={styles.container} >
-                <View style={styles.CircleShapeView}>
+
+              <GestureRecognizer
+                  onSwipeUp={(state) => this.onSwipeUp(state)}
+                  onSwipe={(direction, state) => this.onSwipe(direction, state)}
+                  config={{
+                    velocityThreshold : 0.3,
+                    directionalOffsetThreshold: 80
+                  }}
+                  style={styles.container}>
+                <Animated.View style={[styles.CircleShapeView, { height : this.state.height.interpolate({
+                  inputRange : [0,1],
+                  outputRange : [500, 800]
+                }), transform : [{
+                  translateY: this.state.height.interpolate({
+                    inputRange : [0, 1],
+                    outputRange : [0, -300]
+                  })
+                }] }]}>
                     <Text style={styles.MainText} > Swipe up to start </Text>
-                </View>
-            </View>
+                </Animated.View>
+              </GestureRecognizer>
+
         )
     }
 }
@@ -18,16 +58,20 @@ const styles = StyleSheet.create({
       flex: 1,
       alignItems : "center",
       backgroundColor: '#ffcc00',
-   
+
     },CircleShapeView: {
         marginTop : "100%",
-        width: 500,
         justifyContent : "center",
         alignItems : "center",
-        height: 500,
+        width : 500,
         borderRadius:500/2,
         backgroundColor: '#fff'
     }, MainText : {
         fontSize : 20,
+    },
+    logo : {
+      flex : 1,
+      alignItems : "center",
+      justifyContent : "center"
     }
   });
